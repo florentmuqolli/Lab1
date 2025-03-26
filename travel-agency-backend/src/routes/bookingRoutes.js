68f7588ae789decf4bf3030dffce2e4f48e6721f
florentmuqolli <muqolliflorentt@gmail.com>
2025-03-26 02:29:39 +0100

    Solved an issue that was unknown for weeks
    The users data wouldn't fetch from their own controllers after adding the admin role, using middleware in this way fixed the problem.

--- travel-agency-backend/src/routes/bookingRoutes.js
+++ travel-agency-backend/src/routes/bookingRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 const authMiddleware  = require('../middleware/authMiddleware');
+router.use(authMiddleware(['user']));
 
-router.get('/', authMiddleware, bookingController.getBookingsByUser);
+router.get('/', bookingController.getBookingsByUser);
 
 module.exports = router;
--- travel-agency-backend/src/routes/tourRoutes.js
+++ travel-agency-backend/src/routes/tourRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
 const authMiddleware = require('../middleware/authMiddleware');
+router.use(authMiddleware(['user']));
 
-router.get('/', authMiddleware, tourController.getAllTour);
-router.post('/', authMiddleware, tourController.createTour);
-router.get('/protected', authMiddleware, tourController.getProtectedData);
+router.get('/', tourController.getAllTour);
+router.post('/', tourController.createTour);
+router.get('/protected', tourController.getProtectedData);
 
 module.exports = router;
